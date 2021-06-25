632b321c5a131a768a70302af6167df5b10a7220
TIN-THIET-KE <TIN-THIET-KE@TIN-THIET-KE>
2021-06-25 15:08:49 +0700

    Code Refactor #3

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/export/UserCsvExporter.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/export/UserCsvExporter.java
@@ -13,7 +13,6 @@ import org.supercsv.io.CsvBeanWriter;
 import org.supercsv.io.ICsvBeanWriter;
 import org.supercsv.prefs.CsvPreference;
 
-import com.shopme.admin.user.AbstractExporter;
 import com.shopme.common.entity.User;
 
 public class UserCsvExporter extends AbstractExporter {
