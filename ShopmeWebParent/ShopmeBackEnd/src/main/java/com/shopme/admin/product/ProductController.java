f25528a0084765cd555c6512a8f86db742e50ccf
TIN-THIET-KE <TIN-THIET-KE@TIN-THIET-KE>
2021-07-06 15:21:49 +0700

    Code Pagination, Sort and Filter for Products Page

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/product/ProductController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/product/ProductController.java
@@ -14,6 +14,8 @@ import javax.transaction.Transactional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
@@ -42,18 +44,46 @@ public class ProductController {
 	private BrandService brandService;
 	
 	@GetMapping("/products")
-	public String listAll(Model model) {
-		List<Product> listProducts = productService.listAll();
+	public String listFirstPage(Model model) {
+		return listByPage(1, model, "name", "asc", null);
+	}
+	
+	@GetMapping("/products/page/{pageNum}")
+	public String listByPage(
+				@PathVariable(name="pageNum") int pageNum, Model model,
+				@Param("sortField") String sortField, @Param("sortDir") String sortDir,
+				@Param("keyword") String keyword
+			) {
 		
+		Page<Product> page = productService.listByPage(pageNum, sortField, sortDir, keyword);
+		List<Product> listProducts = page.getContent();
+		
+		long startCount = (pageNum - 1) * ProductService.PRODUCTS_PER_PAGE + 1;
+		long endCount = startCount + ProductService.PRODUCTS_PER_PAGE - 1;
+		if(endCount > page.getTotalElements()) {
+			endCount = page.getTotalElements();
+		}
+		
+		String reverseSortDir = sortDir.equals("asc") ? "desc" : "asc";
+		
+		model.addAttribute("currentPage", pageNum);
+		model.addAttribute("totalPages", page.getTotalPages());
+		model.addAttribute("startCount", startCount);
+		model.addAttribute("endCount", endCount);
+		model.addAttribute("totalItems", page.getTotalElements());
 		model.addAttribute("listProducts", listProducts);
-				
+		model.addAttribute("sortField", sortField);
+		model.addAttribute("sortDir", sortDir);
+		model.addAttribute("reverseSortDir", reverseSortDir);
+		model.addAttribute("keyword", keyword);
+		
 		return "products/products";
 	}
 	
 	@GetMapping("/products/new")
 	public String newProduct(Model model) {
 		List<Brand> listBrands = brandService.listAll();
-		
+		Integer numberOfExistingExtraImages = 0;
 		Product product = new Product();
 		product.setEnabled(true);
 		product.setInStock(true);
@@ -61,6 +91,7 @@ public class ProductController {
 		model.addAttribute("product", product);
 		model.addAttribute("listBrands", listBrands);
 		model.addAttribute("pageTitle", "Create New Product");
+		model.addAttribute("numberOfExistingExtraImages", numberOfExistingExtraImages);
 		
 		return "products/product_form";
 	}
